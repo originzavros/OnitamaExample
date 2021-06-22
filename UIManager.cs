229c86df4285d0ce97967ac5a365b941fc4ada9c
originzavros <77704411+originzavros@users.noreply.github.com>
2021-06-21 20:58:35 -0400

    Add files via upload

---
+++ UIManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Button MenuButton;
+    public GameObject MenuPanel;
+
+    public Button RestartGameButton;    
+    void Start()
+    {
+        EventManager.StartListening("GameOver", OnGameOver);
+    }
+    public GameObject gameOverTxtObject;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnGameOver(string eventName, ActionParams data)
+    {
+        EnableGameOverText();
+    }
+
+    public void EnableGameOverText()
+    {
+        gameOverTxtObject.SetActive(true);
+    }
+    public void DisableGameOverText()
+    {
+        gameOverTxtObject.SetActive(false);
+    }
+
+    public void OnClickMenu()
+    {
+        if(MenuPanel.activeSelf == false)
+        {
+            MenuPanel.SetActive(true);
+        }
+        else{
+            MenuPanel.SetActive(false);
+        }
+    }
+
+    public void OnClickRestartButton()
+    {
+        OnClickMenu();
+        DisableGameOverText();
+        GameManager.RestartGame();
+    }
+
+
+}
